87646043bd025e81853d14b8ad29bcf27c5bcb50
ManishK1832 <mk1169852@gmail.com>
2024-03-12 21:07:11 +0530

    key removed

--- lib/data/remote/api/urls.dart
+++ lib/data/remote/api/urls.dart
@@ -1,7 +1,7 @@
 class Urls{
   static const String BASE_URL = "https://api.openai.com/v1/";
   static const String CHAT_COMPLETION_API = "${BASE_URL}chat/completions";
-  static const String CHAT_COMPLETION_API_KEY = "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";
+  static const String CHAT_COMPLETION_API_KEY = "provide your own key";
 
 
 }
